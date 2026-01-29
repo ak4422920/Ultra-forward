1d2c391a4cb114b9df8b7cf5b6bdd60a8e0f4fa1
ak4422920 <ak4422920@gmail.com>
2026-01-29 07:05:15 +0530

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -9,21 +9,33 @@ class STS:
         self.id = id
         self.data = STATUS
         
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
-    
     def verify(self):
+        """Task ID valid hai ya nahi check karne ke liye."""
         return self.data.get(self.id)
     
-    def store(self, From, to,  skip, limit):
-        self.data[self.id] = {"FROM": From, 'TO': to, 'total_files': 0, 'skip': skip, 'limit': limit,
-                      'fetched': skip, 'filtered': 0, 'deleted': 0, 'duplicate': 0, 'total': limit, 'start': 0}
+    def store(self, From, to, skip, limit):
+        """Task start hote waqt initial data save karta hai."""
+        self.data[self.id] = {
+            "FROM": From, 
+            'TO': to, 
+            'total_files': 0, 
+            'skip': skip, 
+            'limit': limit,
+            'fetched': skip, 
+            'filtered': 0, 
+            'deleted': 0, 
+            'duplicate': 0, 
+            'total': limit, 
+            'start': 0
+        }
         self.get(full=True)
         return STS(self.id)
         
     def get(self, value=None, full=False):
+        """Data fetch karne ke liye."""
         values = self.data.get(self.id)
+        if not values:
+            return None
         if not full:
            return values.get(value)
         for k, v in values.items():
@@ -31,27 +43,48 @@ class STS:
         return self
 
     def add(self, key=None, value=1, time=False):
+        """Counters (fetched/files) ya start time update karne ke liye."""
+        if self.id not in self.data:
+            return
         if time:
           return self.data[self.id].update({'start': tm.time()})
-        self.data[self.id].update({key: self.get(key) + value}) 
+        current_val = self.data[self.id].get(key, 0)
+        self.data[self.id].update({key: current_val + value}) 
     
     def divide(self, no, by):
+       """ETA aur Speed calculation ke liye math helper."""
        by = 1 if int(by) == 0 else by 
        return int(no) / by 
     
     async def get_data(self, user_id):
+        """Database se bot aur user settings fetch karta hai."""
         bot = await db.get_bot(user_id)
-        k, filters = self, await db.get_filters(user_id)
-        size, configs = None, await db.get_configs(user_id)
-        if configs['duplicate']:
-           duplicate = [configs['db_uri'], self.TO]
-        else:
-           duplicate = False
+        configs = await db.get_configs(user_id)
+        filters = await db.get_filters(user_id)
+        
+        # Duplicate detection setup
+        duplicate = [configs['db_uri'], self.get('TO')] if configs['duplicate'] else False
+        
+        # Inline buttons parse karna
         button = parse_buttons(configs['button'] if configs['button'] else '')
-        if configs['file_size'] != 0:
-            size = [configs['file_size'], configs['size_limit']]
-        return bot, configs['caption'], configs['forward_tag'], {'chat_id': k.FROM, 'limit': k.limit, 'offset': k.skip, 'filters': filters,
-                'keywords': configs['keywords'], 'media_size': size, 'extensions': configs['extension'], 'skip_duplicate': duplicate}, configs['protect'], button
- #Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz        
+        
+        # File size limit check
+        size = [configs['file_size'], configs['size_limit']] if configs['file_size'] != 0 else None
+        
+        return (
+            bot, 
+            configs['caption'], 
+            configs['forward_tag'], 
+            {
+                'chat_id': self.get('FROM'), 
+                'limit': self.get('limit'), 
+                'offset': self.get('skip'), 
+                'filters': filters,
+                'keywords': configs['keywords'], 
+                'media_size': size, 
+                'extensions': configs['extension'], 
+                'skip_duplicate': duplicate
+            }, 
+            configs['protect'], 
+            button
+        )
