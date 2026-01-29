934031847dfb84f97b1eb762bd94d7eca0ea11f5
ak4422920 <ak4422920@gmail.com>
2026-01-29 06:58:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,35 +1,28 @@
 import datetime
 from os import environ 
 
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
-
 class Config:
-    API_ID = environ.get("API_ID", "29171167")
+    API_ID = int(environ.get("API_ID", "29171167"))
     API_HASH = environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Lightfrwdbot") 
+    
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://for:for@cluster0.fgu4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5482682830').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002416220645'))
+    
+    # Ye raha aapka backup channel jahan saari forwarded files jayengi
+    ADMIN_BACKUP_CHANNEL = int(environ.get('ADMIN_BACKUP_CHANNEL', '-100xxxxxxxxxx')) 
+    
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/AkmovieVerse") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
-    
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
 
-   
 class temp(object): 
     lock = {}
     CANCEL = {}
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
