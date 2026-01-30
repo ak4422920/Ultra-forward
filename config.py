cffcbb6133101e239d46dfc8dc45dec9a4bfa593
ak4422920 <ak4422920@gmail.com>
2026-01-30 06:16:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,19 @@ import datetime
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "2554744")) # Aapka API ID
+    API_HASH = environ.get("API_HASH", "87545xxxx") # Aapka API HASH
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "AutoXXPostBot") 
     
-    DATABASE_URI = environ.get("DATABASE", "")
+    # Database Variable matching database.py
+    DATABASE_URI = environ.get("DATABASE_URI", environ.get("DATABASE", "")) 
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5482682830').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002416220645'))
     
-    # Ye raha aapka backup channel jahan saari forwarded files jayengi
+    # Backup Channel Feature
     ADMIN_BACKUP_CHANNEL = int(environ.get('ADMIN_BACKUP_CHANNEL', '-1003456547251')) 
     
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/AkmovieVerse") 
@@ -21,8 +22,9 @@ class Config:
     PORT = environ.get('PORT', '8080')
 
 class temp(object): 
-    lock = {}
-    CANCEL = {}
+    lock = {}           # Task locking
+    CANCEL = {}         # Task cancellation
+    DATA = {}           # Task memory storage (Bhot zaroori hai!)
     forwardings = 0
     BANNED_USERS = []
-    IS_FRWD_CHAT = []
+    IS_FRWD_CHAT = []   # Active chat tracking
